c4022b531d43b0393775d1073980f4574115c384
manuelcampagnolo <mlc@isa.ulisboa.pt>
2023-01-30 12:14:37 +0000

    Update concMondego.py

--- python_qgis/introQGIS/concMondego.py
+++ python_qgis/introQGIS/concMondego.py
@@ -1,13 +1,14 @@
 import processing
 
+# seleção por atributos
 processing.run("native:extractbyexpression", 
 {'INPUT':'C:/Users/mlc/OneDrive - Universidade de Lisboa/Documents/temp/linhasAgua.shp',
 'EXPRESSION':' "DESIGNACAO" =  \'Rio Mondego\' OR "DESIGNACAO" =  \'Albufeira Aguieira\' OR "DESIGNACAO" =  \'Albufeira Raiva\' OR "DESIGNACAO" =  \'Albufeira Ponte de Coimbra\' ',
-'OUTPUT':'C:/Users/mlc/OneDrive - Universidade de Lisboa/Documents/temp/RioMondego.gpkg'})
+'OUTPUT':'C:/Users/mlc/OneDrive - Universidade de Lisboa/Documents/temp/Mondego.gpkg'})
 
-
+# seleção por localização
 processing.run("native:extractbylocation", 
 {'INPUT':'C:/Users/mlc/OneDrive - Universidade de Lisboa/Documents/temp/Conc2016.shp',
 'PREDICATE':[0],
-'INTERSECT':'C:/Users/mlc/OneDrive - Universidade de Lisboa/Documents/temp/RioMondego.gpkg',
+'INTERSECT':'C:/Users/mlc/OneDrive - Universidade de Lisboa/Documents/temp/Mondego.gpkg',
 'OUTPUT':'C:/Users/mlc/OneDrive - Universidade de Lisboa/Documents/temp/ConcMondego.gpkg'})
